e70a2b899095d1c29bedb3765bf2d489ec22aebe
ola ola <pella4real@yahoo.com>
2022-08-17 01:48:48 -0400

    version2

--- deploy.py
+++ deploy.py
@@ -1,3 +1,6 @@
 # Deploying a python web application
-echo "Hello World"   = shell scripts    
-print('Hello World') = python script
+#echo "Hello World"
+#= shell scripts
+#python('Hello World')
+#= python script    
+print('Hello World') 
